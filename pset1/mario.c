0842d64482f6bf74ecea73d0dcbbeef0768a6d35
zealsham <shammahagwor@gmail.com>
2016-10-07 02:52:01 +0100

    solved and proper indent

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,51 +1,51 @@
-    #include <stdio.h>
-   #include <cs50.h>
+#include <stdio.h>
+#include <cs50.h>
 
-   int main(){
-      int userInput,i,space,hashes,check;
+int main() {
+    int userInput,i,space,hashes,check;
 
-   do{
-       printf("Height: ");
+    do {
+        printf("Height: ");
 
-      userInput=GetInt();
+        userInput=GetInt();
 
-      if(userInput==0){
-          return 0;
-      }
+        if(userInput==0) {
+            return 0;
+        }
 
-      }while(userInput<1 || userInput>23);
+    } while(userInput<1 || userInput>23);
 
-      if(userInput==1){
+    if(userInput==1) {
 
-          printf("##\n");
-      }
-
-      else{
-
-      space=userInput-2;
-
-      hashes=userInput-space;
-      check=1;
-
-   while(check<=userInput){
-
-      for(i=0; i<space+1; i++){
-
-        printf(" ");
-
-      }
-      for( i = 0; i < hashes; i++) {
-              printf("#");
-
-     }
-
-       printf("\n");
-        hashes++;
-        space--;
-        check++;
+        printf("##\n");
     }
 
-      }
+    else {
 
-      return 0;
-   }
+        space=userInput-2;
+
+        hashes=userInput-space;
+        check=1;
+
+        while(check<=userInput) {
+
+            for(i=0; i<space+1; i++) {
+
+                printf(" ");
+
+            }
+            for( i = 0; i < hashes; i++) {
+                printf("#");
+
+            }
+
+            printf("\n");
+            hashes++;
+            space--;
+            check++;
+        }
+
+    }
+
+    return 0;
+}
--- pset1/water.c
+++ pset1/water.c
@@ -4,17 +4,17 @@
 int main(void) {
     int bathTime,bottles;
     float showerGallon,showerOunce;
-    
+
     printf("minutes: ");
-     bathTime=GetInt();
-     
-     showerGallon=bathTime*1.5;
-     
-     showerOunce=showerGallon*128;
-     bottles=showerOunce/16;
-     
-     printf("bottles: %d",bottles);
-     
-     return 0;
-    
+    bathTime=GetInt();
+
+    showerGallon=bathTime*1.5;
+
+    showerOunce=showerGallon*128;
+    bottles=showerOunce/16;
+
+    printf("bottles: %d",bottles);
+
+    return 0;
+
 }
